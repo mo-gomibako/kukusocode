f9b8b639d43d079d4633518856ccaa977ec4c23b
mogmoogles <mo1419091@gmail.com>
2019-05-01 18:26:39 +0900

    I used '+='.

--- index2.js
+++ index2.js
@@ -1,13 +1,13 @@
 var a = "\t"
 for(var b = 1; b < 10; b++){
-  a = `${a}${b}\t`
+  a += `${b}\t`
 }
-a = `${a}\n`
+a += `\n`
 for(var d = 1; d < 10; d++){
-  a = `${a}${d}\t`
+  a  += `${d}\t`
   for(var c = 1; c < 10; c++){
-    a = `${a}${c * d}\t`
+    a += `${c * d}\t`
   }
-  a = `${a}\n`
+  a += `\n`
 }
 console.log(a)
---
+++ test.js
@@ -0,0 +1,5 @@
+var a = ""
+for(var b = 1; b < 10; b++){
+    a = `${a}${b}`
+}
+console.log(a)
